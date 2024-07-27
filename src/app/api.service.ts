b6ce14bc1873508dce7ecf87c96404190adc73b8
Mauricio Herrera <johnmauricio.herrera214@comunidadunir.net>
2024-07-27 14:48:59 -0500

    ajustes despliegue

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,34 +1,37 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private  http: HttpClient) { }
-  private apiUrl = "http://ec2-3-144-124-56.us-east-2.compute.amazonaws.com/backQuienloquiere/";
-  private validateUser =  this.apiUrl + 'user/login?_format=json';
-  private registerUser =  this.apiUrl + 'jsonapi/user/user';
-  private logoutUser =  this.apiUrl + 'user/logout';
-  private statusUser =  this.apiUrl + 'user/login_status?_format=json';
-  private exitUser =  this.apiUrl + 'user/logout?_format=json&token=';
+  private apiUrl = environment.apiUrl;
+  private validateUser = this.apiUrl + 'user/login?_format=json';
+  private registerUser = this.apiUrl + 'jsonapi/user/user';
+  private logoutUser = this.apiUrl + 'user/logout';
+  private statusUser = this.apiUrl + 'user/login_status?_format=json';
+  private exitUser = this.apiUrl + 'user/logout?_format=json&token=';
   private newProduct = this.apiUrl + 'jsonapi/node/productos';
-  private deleteProduct =  this.apiUrl + 'jsonapi/node/productos/';
+  private deleteProduct = this.apiUrl + 'jsonapi/node/productos/';
 
   private headers = new HttpHeaders({
     'Content-Type': 'application/vnd.api+json'
   });
   private headersToken: HttpHeaders | undefined;
-  validateUserFunction(user:any,pass:any) {
+
+  constructor(private http: HttpClient) { }
+
+  validateUserFunction(user: any, pass: any) {
     const data = {
       "name": user,
       "pass": pass
-    }
-    return this.http.post(this.validateUser, data, { headers: this.headers,withCredentials: true  });
+    };
+    return this.http.post(this.validateUser, data, { headers: this.headers, withCredentials: true });
   }
 
-  registerUserFunction(user:any,pass:any, email:any, correo:any, avatar:any) {
+  registerUserFunction(user: any, pass: any, email: any, correo: any, avatar: any) {
     const dataTem = {
       type: "user--user",
       attributes: {
@@ -36,37 +39,35 @@ export class ApiService {
         mail: email,
         field_correo: correo,
         pass: pass,
-        status:1,
-        field_avatar:avatar
+        status: 1,
+        field_avatar: avatar
       }
-    }
+    };
     const userData = {
-      data:dataTem
-    }
+      data: dataTem
+    };
     const base64Credentials = btoa('creador' + ':' + 'D9yVmh4yVcRP2Be');
     const headers = new HttpHeaders({
       'Authorization': 'Basic ' + base64Credentials,
       'Content-Type': 'application/vnd.api+json'
     });
-    return this.http.post(this.registerUser, userData, { headers:headers,withCredentials: true  });
+    return this.http.post(this.registerUser, userData, { headers: headers, withCredentials: true });
   }
 
   logoutUserFunction() {
-console.log('logoutUserFunction');
-      const logoutToken = localStorage.getItem('csrf_token');
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json'
-      });
-      // @ts-ignore
-      const params = new HttpParams().set('_format', 'json').set('csrf_token', logoutToken);
-      return this.http.post<any>( this.logoutUser, {}, { headers, params ,withCredentials: true });
+    console.log('logoutUserFunction');
+    const logoutToken = localStorage.getItem('csrf_token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    const params = new HttpParams().set('_format', 'json').set('csrf_token', logoutToken || '');
+    return this.http.post<any>(this.logoutUser, {}, { headers, params, withCredentials: true });
+  }
 
-    }
-
-  createEventFunction(data: any){
+  createEventFunction(data: any) {
     const username = localStorage.getItem('user');
-    const password =  localStorage.getItem('pass');
-    const token =  localStorage.getItem('csrf_token');
+    const password = localStorage.getItem('pass');
+    const token = localStorage.getItem('csrf_token');
     const base64Credentials = btoa(username + ':' + password);
     if (token !== null) {
       this.headersToken = new HttpHeaders({
@@ -75,14 +76,13 @@ console.log('logoutUserFunction');
         'X-CSRF-Token': token
       });
     }
-    return this.http.post(this.newProduct, data, { headers: this.headersToken,withCredentials: true  });
-
+    return this.http.post(this.newProduct, data, { headers: this.headersToken, withCredentials: true });
   }
 
   deleteProductos(nid: number) {
     const username = localStorage.getItem('user');
-    const password =  localStorage.getItem('pass');
-    const token =  localStorage.getItem('csrf_token');
+    const password = localStorage.getItem('pass');
+    const token = localStorage.getItem('csrf_token');
     const base64Credentials = btoa(username + ':' + password);
 
     if (token !== null) {
@@ -92,10 +92,6 @@ console.log('logoutUserFunction');
         'X-CSRF-Token': token
       });
     }
-    return this.http.delete(this.deleteProduct+nid, { headers: this.headersToken,withCredentials: true  });
-
+    return this.http.delete(this.deleteProduct + nid, { headers: this.headersToken, withCredentials: true });
   }
-
-
 }
-
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://ec2-3-144-124-56.us-east-2.compute.amazonaws.com/backQuienloquiere/' // Reemplaza con tu URL de producción
+};
